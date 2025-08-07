ccd894d271a086b74a79a7e9ee0889a150b82f79
yogesh <yogirao8440@gmail.com>
2025-08-07 20:52:31 +0530

    fix: home link added

--- src/app/Components/Header/User.jsx
+++ src/app/Components/Header/User.jsx
@@ -2,11 +2,12 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
 import LogoutBtn from './LogoutBtn';
-
+import Link from 'next/link';
 export default function User() {
     const { data } = useSession();
     return (
         <div id="user-info-header" className={`flex items-center space-x-4 mt-4 sm:mt-0 ${!data && 'hidden'}`}>
+              <Link href="/dashboard" className="text-blue-600 text-lg font-semibold">Home</Link>
             <p className="text-lg text-gray-700">Welcome,  <span id="username-display" className="font-semibold text-blue-600">{data?.user?.name || "User"}</span> </p>
             <LogoutBtn />
         </div>
